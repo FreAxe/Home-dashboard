404607505802260a90b9925afd28bdd8561123bb
FreAxe <freddy_94_@hotmail.com>
2025-05-14 15:46:03 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,13 +2,16 @@ const devices = {
   "Vardagsrum": { status: false },
   "Kök": { status: false }
 };
+
 function updateStatusUI(name) {
   const device = devices[name];
   const el = document.querySelector(`#device-${name.toLowerCase()} .status`);
   el.innerText = device.status ? "På" : "Av";
 }
+
 function toggle(name) {
   devices[name].status = !devices[name].status;
   updateStatusUI(name);
 }
+
 Object.keys(devices).forEach(updateStatusUI);
